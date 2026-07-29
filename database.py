cf64f1ec78d42b608a21855c55ce07b3a1bfc487
Мага <Akharashevw@mail.ru>
2026-07-29 14:20:05 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,3 +1,38 @@
+import json
+
+FILE = "stock.json"
+
+
+def load_stock():
+    try:
+        with open(FILE, "r", encoding="utf-8") as f:
+            return json.load(f)
+    except:
+        return []
+
+
+def save_stock(stock):
+    with open(FILE, "w", encoding="utf-8") as f:
+        json.dump(stock, f, ensure_ascii=False, indent=4)
+
+
+def add_tire(brand, size, season, quantity):
+    stock = load_stock()
+
+    stock.append({
+        "brand": brand,
+        "size": size,
+        "season": season,
+        "quantity": quantity
+    })
+
+    save_stock(stock)
+
+
+def get_stock():
+    return load_stock()
+
+
 def remove_tire(brand, size, season, quantity):
     stock = load_stock()
 
