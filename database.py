6811ec719c332df5361267ef6c2536c9c69337c5
Мага <Akharashevw@mail.ru>
2026-07-29 14:10:38 +0300

    Update database.py

--- database.py
+++ database.py
@@ -31,3 +31,21 @@ def add_tire(brand, size, season, quantity):
 
 def get_stock():
     return load_stock()
+    def remove_tire(brand, size, season, quantity):
+    stock = load_stock()
+
+    for tire in stock:
+        if (
+            tire["brand"] == brand
+            and tire["size"] == size
+            and tire["season"] == season
+        ):
+            tire["quantity"] -= quantity
+
+            if tire["quantity"] <= 0:
+                stock.remove(tire)
+
+            save_stock(stock)
+            return True
+
+    return False
