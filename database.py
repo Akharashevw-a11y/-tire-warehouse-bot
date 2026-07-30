1de2d9566ebb17870f254f667fcb7a6bbae41e38
Мага <Akharashevw@mail.ru>
2026-07-30 09:50:21 +0300

    Update database.py

--- database.py
+++ database.py
@@ -9,12 +9,14 @@ def load_stock():
             stock = json.load(f)
 
             for tire in stock:
-
                 if "photo" not in tire:
                     tire["photo"] = None
 
                 if "price" not in tire:
                     tire["price"] = 0
+
+                if "type" not in tire:
+                    tire["type"] = ""
 
             return stock
 
@@ -22,11 +24,8 @@ def load_stock():
         return []
 
 
-
 def save_stock(stock):
-
     with open(FILE, "w", encoding="utf-8") as f:
-
         json.dump(
             stock,
             f,
@@ -35,11 +34,10 @@ def save_stock(stock):
         )
 
 
-
 def add_tire(
     brand,
     size,
-    season,
+    tire_type,
     quantity,
     price,
     photo=None
@@ -47,59 +45,43 @@ def add_tire(
 
     stock = load_stock()
 
-
     stock.append({
-
         "brand": brand,
         "size": size,
-        "season": season,
+        "type": tire_type,
         "quantity": quantity,
         "price": price,
         "photo": photo
-
     })
-
 
     save_stock(stock)
 
 
-
 def get_stock():
-
     return load_stock()
 
 
-
-def remove_tire(brand, size, season, quantity):
+def remove_tire(brand, size, tire_type, quantity):
 
     stock = load_stock()
-
 
     for tire in stock:
 
         if (
-
             tire["brand"].lower() == brand.lower()
             and tire["size"].lower() == size.lower()
-            and tire["season"].lower() == season.lower()
-
+            and tire["type"].lower() == tire_type.lower()
         ):
 
             tire["quantity"] -= quantity
 
-
             if tire["quantity"] <= 0:
-
                 stock.remove(tire)
 
-
             save_stock(stock)
-
             return True
 
-
     return False
-
 
 
 def find_tires(query):
@@ -108,22 +90,17 @@ def find_tires(query):
 
     result = []
 
-
-    query = query.lower()
-
+    words = query.lower().split()
 
     for tire in stock:
 
-        if (
+        text = (
+            f"{tire['brand']} "
+            f"{tire['size']} "
+            f"{tire['type']}"
+        ).lower()
 
-            query in tire["brand"].lower()
-            or query in tire["size"].lower()
-            or query in tire["season"].lower()
-
-        ):
-
+        if all(word in text for word in words):
             result.append(tire)
-
-
 
     return result
